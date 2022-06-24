6410f3181e8295d494c5844c5ccd26c5ca1d6362
Reneé González-Guzmán <103365172+TheRFrog@users.noreply.github.com>
2022-06-23 19:25:17 -0500

    Delete R directory

--- R/shinyNORRRM-package.R
+++
@@ -1,9 +0,0 @@
-#' For greater control over your plots, use the R command line;
-#' for usage instructions, consult the guidance article
-#' [online](https://github.com/TheRFrog/shinyNORRRM) or by
-#'
-#' @keywords internal
-"_PACKAGE"
-## usethis namespace: start
-## usethis namespace: end
-NULL
